abe3c46c6d3d836f07e69f8fb6a0905c889b710f
Ornã Keivison Fernandes Câmara <ornakeivisonf@gmail.com>
2025-03-07 08:53:15 -0300

    inicio login

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from crud import read
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
