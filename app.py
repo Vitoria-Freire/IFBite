a1060832f494232f9c282ce947866efff32e43e6
Ornã Keivison Fernandes Câmara <ornakeivisonf@gmail.com>
2025-03-08 17:05:21 -0300

    adição de cadastre de usuarios com restaurantes

--- app.py
+++ app.py
@@ -22,18 +22,50 @@ def cadastro():
         telefone = request.form.get('tel')
         tipoUsuario = request.form.get('tipoUsuario')
         
-        if tipoUsuario == 'cli' or tipoUsuario == 'ent':
-            if createUpdateDelete(
-                "INSERT INTO usuario (email, senha, nome, telefone, tipo) VALUES (%s,%s,%s,%s,%s)",
-                (email, senha, nome, telefone, tipoUsuario),
-                "INSERT"):
+        if createUpdateDelete(
+            "INSERT INTO usuario (email, senha, nome, telefone, tipo) VALUES (%s,%s,%s,%s,%s)",
+            (email, senha, nome, telefone, tipoUsuario),
+            "INSERT"):
+
+            if tipoUsuario == 'res':
+                id = read(
+                "SELECT id FROM usuario WHERE nome = %s",
+                (nome, ))
+
+                return redirect(url_for('cadastroRestaurante', id = int(id[0]['id'])))
+    
+            return render_template('sucesso.html', mensagem = "Você foi Cadastrado com Sucesso!", url = "/login")
         
-                return render_template('sucesso.html', mensagem = "Você foi Cadastrado com Sucesso!", url = "/")
-            
-            else:
-                return render_template('erro.html', mensagem = "Ocorreu um erro no seu Cadastro, Tente Novamente!", url = "/cadastro")
-            
-
-
+        else:
+            return render_template('erro.html', mensagem = "Ocorreu um erro no seu Cadastro, Tente Novamente!", url = "/cadastro")
 
     return render_template('cadastro.html')
+
+@app.route('/cadastroRestaurante<int:id>', methods=['GET', 'POST'])
+def cadastroRestaurante(id):
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        telefone = request.form.get('tel')
+        cnpj = request.form.get('cnpj')
+        localidade = request.form.get('localidade')
+
+        if createUpdateDelete(
+            "INSERT INTO restaurante (nome, telefone, cnpj, localidade) VALUES (%s,%s,%s,%s)",
+            (nome, telefone, cnpj, localidade),
+            "INSERT"):
+
+            idRestaurante = read(
+                "SELECT id FROM restaurante WHERE nome = %s",
+                (nome, ))
+            
+            createUpdateDelete(
+                "UPDATE usuario SET idRestaurante = %s WHERE id = %s",
+                (int(idRestaurante[0]['id']), id),
+                "UPDATE")
+    
+            return render_template('sucesso.html', mensagem = "Você Cadastrou se Restaurante com Sucesso!", url = "/login")
+        
+        else:
+            return render_template('erro.html', mensagem = "Ocorreu um erro no Cadastro, Tente Novamente!", url = "/cadastroRestaurante")
+
+    return render_template('cadastroRestaurante.html')
