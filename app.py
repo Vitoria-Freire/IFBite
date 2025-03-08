9fa27d50d9f0850d6f1805bf51b0b41f1e7c7d9c
Ornã Keivison Fernandes Câmara <ornakeivisonf@gmail.com>
2025-03-08 15:26:23 -0300

    inclusão de paginas de sucesso e erro!

--- app.py
+++ app.py
@@ -21,13 +21,19 @@ def cadastro():
         nome = request.form.get('nome')
         telefone = request.form.get('tel')
         tipoUsuario = request.form.get('tipoUsuario')
+        
+        if tipoUsuario == 'cli' or tipoUsuario == 'ent':
+            if createUpdateDelete(
+                "INSERT INTO usuario (email, senha, nome, telefone, tipo) VALUES (%s,%s,%s,%s,%s)",
+                (email, senha, nome, telefone, tipoUsuario),
+                "INSERT"):
+        
+                return render_template('sucesso.html', mensagem = "Você foi Cadastrado com Sucesso!", url = "/")
+            
+            else:
+                return render_template('erro.html', mensagem = "Ocorreu um erro no seu Cadastro, Tente Novamente!", url = "/cadastro")
+            
 
-        print(email, senha, nome, telefone, tipoUsuario)
 
-        createUpdateDelete(
-            "INSERT INTO usuario (email, senha, nome, telefone, tipo) VALUES (%s,%s,%s,%s,%s)",
-            (email, senha, nome, telefone, tipoUsuario),
-            "INSERT"
-        )
 
     return render_template('cadastro.html')
